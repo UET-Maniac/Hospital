80bc2ee739693bcefb9ddc7ae3cfe2833b7ce87c
HoangPhu98 <hoangvanphu98@gmail.com>
2018-04-08 21:42:54 +0700

    edit images of use case

--- SourceCode/Hospital-E/routes/index.js
+++ SourceCode/Hospital-E/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/index');
+    res.render('pages/index', { what: "Hello" });
 });
 
 module.exports = router;
