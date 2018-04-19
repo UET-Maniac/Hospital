98eba217ea26507dc781982c30540933199f1d1d
HoangPhu98 <hoangvanphu98@gmail.com>
2018-04-19 21:47:29 +0700

    few edit

--- SourceCode/Hospital-E/app.js
+++ SourceCode/Hospital-E/app.js
@@ -14,6 +14,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var userRouter = require('./routes/user');
 var loginRouter = require('./routes/login');
+var newsRouter = require('./routes/news');
 var infoDepartmentRouter = require('./routes/informations/information.department');
 var infoDoctorRouter = require('./routes/informations/information.doctor');
 
@@ -45,6 +46,7 @@ app.use('/', indexRouter);
 app.use('/login', loginRouter)
 app.use('/users', usersRouter);
 app.use('/user', userRouter);
+app.use('/news', newsRouter);
 app.use('/infoDepartments', infoDepartmentRouter);
 app.use('/infoDoctors', infoDoctorRouter);
 
--- SourceCode/Hospital-E/routes/login.js
+++ SourceCode/Hospital-E/routes/login.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+// var Account = require('../models/accountModel');
 var User = require("../models/user.model.js")
 
 
@@ -14,24 +16,57 @@ router.route('/')
             password: req.body.password
         }
         User.login(data, function(err, user) {
-            if(err){
+            if (err) {
                 //
                 res.status(500).json({
                     message: 'Error with server'
                 })
-            } else if(user){
+            } else if (user) {
                 // Có cách nào mà có hay không có {user: user} index.ejs nó không lỗi không nhỉ?
                 // Với lại có cách nào mà cái thông tin user sau đăng nhập được gắn vô head
                 // để thuận tiện cho việc check trong các phần sau không nhỉ?
                 // res.render("pages/index", {user: user});
                 res.render("pages/index");
-            } else{
+            } else {
                 //
                 res.status(404).json({
                     message: 'User name or password is wrong'
                 })
             }
         })
-    })
+    });
+
+
+// router.get('/', (req, res, next) => {
+//     res.render('pages/login');
+// });
+
+// router.post('/', (req, res, next) => {
+//     var data = {
+//         email: req.body.email,
+//         pass: req.body.pass
+//     };
+//     Account.find({ 'email': data.email, 'pass': data.pass })
+//         .exec()
+//         .then(doc => {
+//             console.log("From database:", doc);
+//             if (doc) {
+//                 res.status(200).json({
+//                     products: doc,
+//                     request: {
+//                         type: 'GET',
+//                         url: 'https://localhost:3000/login/'
+//                     }
+
+//                 });
+//             } else {
+//                 res.status(404).json({ message: "No valid entru found!" });
+//             }
+//         })
+//         .catch(err => {
+//             console.log(err);
+//             res.status(500).json({ error: err });
+//         });
+// });
 
 module.exports = router;
