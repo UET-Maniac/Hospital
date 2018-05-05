58e5673a5b1f33035cd5b9208693dbe6c718f8ff
HoangPhu98 <hoangvanphu98@gmail.com>
2018-05-05 12:38:04 +0700

    call data in news

--- SourceCode/Hospital-E/routes/news.js
+++ SourceCode/Hospital-E/routes/news.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var config = require('../config.json');
 var objectType = config.viewer;
+var News = require("../models/news");
 
 router.use(function(req, res, next){
     // console.log(req.objectType)
@@ -15,7 +16,15 @@ router.use(function(req, res, next){
 
 /* GET page News */
 router.get('/', function(req, res, next) {
-    res.render('pages/news', {objectType: objectType});
+    News.find({}, (err, news_data) => {
+        console.log(news_data);
+        if(err || !news_data.length){
+            res.render('pages/error',
+			        { objectType: config.viewer, message: 'Không tìm thấy dữ liệu phù hợp!', codeError: 404});
+        }else{
+            res.render('pages/news', {news: news_data, objectType: objectType});
+        }
+    });
 });
 
 module.exports = router;
