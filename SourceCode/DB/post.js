2d15b9bf0ec48426515c855f02963db2b66f9a36
phamconganh <conganhphamnd@gmail.com>
2018-05-05 12:35:31 +0700

    update db version 5

--- SourceCode/DB/post.js
+++ SourceCode/DB/post.js
@@ -1,242 +1,69 @@
 var arr = [
 	{
 		"_id": "POS10000001",
-		"content": "Đây ko phải là bài post",
-		"image": "",
+		"tags": ["Tư vấn"],
+		"title": "Những việc cần làm khi bị chấn thương sọ não",
+		"content": "Vì việc chụp CT sọ não không thực sự cần thiết phải tiến hành ngay nên chúng ta có thể trì hoãn. Thay vì việc phải đưa bệnh nhân bằng được lên tuyến trên hoặc các cơ sở lớn có máy chụp CT thì cần đưa bệnh nhân vào bệnh viện đa khoa gần nhất, có khoa chấn thương, khoa ngoại thần kinh hoặc có phòng hồi sức cấp cứu để được khám và chẩn đoán sơ bộ. Từ đây, bác sĩ sẽ đưa ra những phán đoán rất chính xác và hướng dẫn bạn tận tình các công việc cần làm tiếp theo với chi phí rẻ hơn rất nhiều lần chụp CT. Hiệu quả với điều trị lại cực hữu dụng. Cũng lưu ý khi vận chuyển nạn nhân, cần cho nạn nhân nằm ngửa, đầu nghiêng về một bên và không gối cao. Nhớ nằm trên ván cứng để tránh các trường hợp tổn thương cột sống kèm theo thì sẽ không bị biến chứng sang tủy sống, nặng thêm. Tất cả mọi vết thương chảy máu ngoài da cần phải được băng bó, cầm máu cẩn thận. Không tiến hành chuyển đi mà chưa kịp làm tốt khâu này. Bạn có thể bảo toàn tính mạng cho người bệnh tốt nhất chỉ bằng một vài phút băng bó. Trong mọi trường hợp, không để người bệnh đi bộ, ngồi dậy hoặc chạy nhảy khắp nơi. Vì như vậy có thể làm bong các nút máu đông tạm thời và chuyển từ một ổ đụng giập não không có chảy máu sang một ổ chảy máu sọ não hoàn chỉnh. Lúc này, bạn sẽ không thể bảo toàn được mức độ bệnh cho người bệnh. Không vận động mạnh, không xúc động mạnh, không có các kích thích thần kinh, theo dõi liên tục từ 7 - 10 ngày. Kịp thời phát hiện ngay tất cả các dấu hiệu bất thường để xử lý ngay lập tức là cách hữu dụng nhất bạn nên tiến hành làm thay vì chỉ chú ý vào việc huy động tiền của để chụp cho được một cái phim CT sọ não.",
+		"image": "/images/datas/POST10000001.jpg",
 		"userId": "USE100000084",
+		"postType": 1,
 		"subPostIds": []
 	},
 	{
 		"_id": "POS10000002",
-		"content": "Post cái gì lên cho hợp lí đây",
-		"image": "",
+		"tags": ["Tin tức"],
+		"title": "​Bệnh về mắt thường gặp và cách phòng tránh",
+		"content": "Những bệnh liên quan đến mắt thường gặp hiện nay như: cận thị, khô mắt, đục thủy tinh thể… đang có dấu hiệu gia tăng với những biến chứng suy giảm thị lực, thậm chí mù lòa. Để phòng tránh bệnh mắt hiệu quả:  Kiểm tra mắt định kỳ sáu tháng một lần là cách tốt nhất giúp sớm phát hiện những bệnh về mắt để điều trị kịp thời, nên đi khám mắt khi có biểu hiện lạ, khi đi ra ngoài nắng nên đeo kính râm để bảo vệ mắt, tránh tia cực tím và tránh được bệnh đục thủy tinh thể (cườm mắt), luôn đọc sách báo dưới ánh sáng tốt để tránh căng thẳng cho mắt, cứ sau nửa tiếng làm việc bằng mắt nên cho mắt nghỉ bằng cách nhìn chỗ khác hoặc nhìn vật thể ở xa, chớp mắt và mát-xa mắt.",
+		"image": "images/datas/POST10000002.jpg",
 		"userId": "USE100000016",
+		"postType": 0,
 		"subPostIds": []
 	},
 
 	{
 		"_id": "POS10000003",
-		"content": "Post thử cho vui",
-		"image": "",
+		"tags": ["Tin tức"],
+		"title": "Thoái hóa cột sống - nguồn cơn gây ra các bệnh xương khớp",
+		"content": "Thoái hóa cột sống là bệnh lý khá phổ biến hiện nay, đặc biệt là ở người trung niên và cao tuổi. Nguyên nhân dẫn đến thoái hóa cột sống có thể chia làm hai loại nguyên phát và thứ phát. Nguyên nhân nguyên phát chính là quá trình lão hóa tự nhiên của cơ thể khiến cho cột sống không còn giữ được cấu trúc vững chắc và ổn định như ban đầu. Nguyên nhân thứ phát bao gồm các tác động từ bên ngoài như chấn thương, lao động nặng, chế độ ăn uống không khoa học, thừa cân béo phì… Thoái hóa cột sống có thể ảnh hưởng đến tất cả các bộ phận trong cơ thể cũng như chất lượng cuộc sống của người bệnh. Khi cột sống thoái hóa đến một mức độ nhất định sẽ kéo theo đĩa đệm mất nước, xẹp lún, dây chằng cũng bị xơ và mòn. Đây cũng chính là nguyên nhân hàng đầu dẫn đến những căn bệnh vô cùng phổ biến hiện nay như thoát vị đĩa đệm, gai cột sống, trượt đốt sống... Nếu không được điều trị đúng cách, những căn bệnh này sẽ diễn tiến rất nhanh gây đau nặng dai dẳng, làm giới hạn chuyển động của cơ thể, giảm khả năng lao động. Nghiêm trọng hơn, nhiều bệnh nhân còn phải đối mặt với nguy cơ liệt nửa người, tàn phế do áp lực chèn ép quá sâu lên rễ thần kinh. Chính vì thế, có thể nói điều trị sớm thoái hóa cột sống là chìa khóa giữ sức khỏe cột sống lâu dài.",
+		"image": "images/datas/POST10000003.jpg",
 		"userId": "USE100000094",
+		"postType": 0,
 		"subPostIds": []
 	},
 
 	{
 		"_id": "POS10000004",
-		"content": "Post được 1 like mai nghỉ học",
-		"image": "",
+		"tags": ["Hỏi đáp"],
+		"title": "Vì sao màng nhĩ dễ bị thủng?",
+		"content": "Màng nhĩ là một mắt xích không thể thiếu trong hệ thống dẫn truyền âm thanh nhưng lại mang một vóc dáng rất liễu yếu đào tơ nên rất dễ bị tổn thương. Màng nhĩ chắn ngang tạo sự ngăn cách hoàn toàn giữa ống tai ngoài và tai giữa, cho nên nếu một ai đó mà… xì khói được ra tai thì chứng tỏ người đó đã bị thủng màng nhĩ…từ lâu. Màng này mỏng, bán trong suốt có hình elip và hơi lõm vào trong. Do màng nhĩ rất mỏng nên khi ngoáy tai, ráy tai hoặc lấy dị vật trong tai nếu không cẩn thận sẽ có nguy cơ làm thủng màng nhĩ. Khi phẫu thuật xương bàn đạp trong tai, hoặc trong chấn thương có nứt xương sọ thái dương cũng có thể làm rách màng nhĩ. Màng nhĩ giữ một khâu quan trọng trong dây chuyền thính giác cho nên để bảo vệ nó, trước tiên cần phải hạn chế thói quen ngoáy tai và tự lấy ráy tai cũng như phải phản ứng nhanh để tránh được những cú đập không mong muốn vào chỗ treo khuyên. Khi thấy triệu chứng tắc vòi nhĩ, ù và đau trong tai thì phải đi khám để được điều trị kịp thời, tránh cái họa viêm mủ tai giữa gây biến chứng mà chí ít cũng là thủng màng nhĩ lâu lành.",
+		"image": "images/datas/POST10000004.jpg",
 		"userId": "USE100000035",
+		"postType": 1,
 		"subPostIds": []
 	},
 
 	{
 		"_id": "POS10000005",
-		"content": "Không tiêu đề",
-		"image": "",
+		"tags": ["Tư vấn"],
+		"title": "Đau cổ vai và cách tập luyện",
+		"content": "Đây thường là hậu quả của hiện tượng quá tải cơ vùng cổ gáy, thoái hóa cột sống cổ, thoát vị đĩa đệm cột sống cổ. Bệnh rất hay gặp ở những người phải làm việc ở tư thế ngồi nhiều (làm với máy tính, nghề may…), làm việc trong trạng thái rung sóc (lái xe), các bất lợi cho cột sống trong sinh hoạt cũng như lao động khác (ngồi lệch vẹo cổ khi làm việc, mang, sách nặng lệch một bên…). Diễn biến của bệnh kéo dài gây khó chịu trong sinh hoạt, khó tập trung trong công việc, các biến chứng hẹp ống sống do thoát vị hoặc do thoái hóa cũng có thể gây yếu tay chân. Để phòng và hỗ trợ điều trị thì việc luyện tập là rất cần thiết, nên tiến hành song song với điều trị nguyên nhân gây bệnh. Cách tập: Tập xoay cổ, tập nghiêng cổ hai bên,tập cúi ngửa cổ, tập mạnh cơ cổ,...",
+		"image": "images/datas/POST10000005.jpg",
 		"userId": "USE100000026",
+		"postType": 1,
 		"subPostIds": []
 	},
 
 	{
 		"_id": "POS10000006",
-		"content": "Bệnh viện làm ăn chán quá",
+		"tags": ["Tin tức"],
+		"title": "Trị viêm mũi xoang theo phương pháp Nhật Bản",
+		"content": "Viêm xoang là tình trạng niêm mạc xoang bị viêm, tổn thương, khiến cho niêm mạc xoang không hoạt động bình thường được. Tình trạng tổn thương lâu ngày dẫn đến ứ đọng các dịch nhầy bẩn. Chất dịch này bám vào thành hốc xoang, lấp đầy hốc xoang, làm hẹp, tắc lỗ thông xoang, dẫn đến viêm nhiễm tạo mủ. Tại Việt Nam hiện nay, người tiêu dùng có thể chọn mua bộ sản phẩm vệ sinh mũi Rohto NoseWash được nghiên cứu và sản xuất theo Công Nghệ Nhật Bản. Sản phẩm này có kèm theo Bình rửa mũi chuyên dụng nhỏ gọn, giúp việc rửa mũi dễ dàng và an toàn hơn. Dung dịch rửa mũi Rohto NoseWash có thành phần chính là: muối biển Nhật Bản, Benzalkonium Chloride, các dưỡng chất Kẽm và Đồng… được nghiên cứu với độ pH phù hợp với sinh lý khoang mũi, nên hoàn toàn không đau rát khi rửa.",
 		"image": "",
 		"userId": "USE100000017",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000007",
-		"content": "Tại sao tôi đã uống thuốc mà bệnh đã khỏi?",
-		"image": "",
-		"userId": "USE100000083",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000008",
-		"content": "Bác sĩ éo có lương tâm gì cả",
-		"image": "",
-		"userId": "USE100000052",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000009",
-		"content": "Ko được 10 like thì nghỉ việc",
-		"image": "",
-		"userId": "USE100000011",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000010",
-		"content": "Nhiều người đòi đóng cửa bệnh viện quá",
-		"image": "",
-		"userId": "USE100000047",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000011",
-		"content": "Haizz, chả biết viết gì nữa",
-		"image": "",
-		"userId": "USE100000062",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000012",
-		"content": "Chữa bệnh tốn kém mà ko ăn thua",
-		"image": "",
-		"userId": "USE100000098",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000013",
-		"content": "Đây ko biết có phải bệnh viện hay ko nữa",
-		"image": "",
-		"userId": "USE100000059",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000014",
-		"content": "Bệnh nhân éo gì toàn dọa giết bác sĩ là sao",
-		"image": "",
-		"userId": "USE100000008",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000015",
-		"content": "Cơ sở kém quá, toàn phải ngủ dưới gầm giường",
-		"image": "",
-		"userId": "USE100000078",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000016",
-		"content": "Mỗi lần tiêm toàn phải mất 5 lít ko thì nát ven",
-		"image": "",
-		"userId": "USE100000076",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000017",
-		"content": "Bệnh nhân đã ko có bảo hiểm lại còn kêu viện phí đắt",
-		"image": "",
-		"userId": "USE100000012",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000018",
-		"content": "Mai chắc xin chuyển viện",
-		"image": "",
-		"userId": "USE100000060",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000019",
-		"content": "Đã gãy chân mà cứ bảo tập chạy bộ 7 ngày 1 tuần là sao",
-		"image": "",
-		"userId": "USE100000040",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000020",
-		"content": "Ông trưởng khoa đòi phong bì nhiều thế",
-		"image": "",
-		"userId": "USE100000083",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000021",
-		"content": "Ai ko có giường nằm điểm danh cái",
-		"image": "",
-		"userId": "USE100000059",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000022",
-		"content": "Mình chọn sai nghề cmnr",
-		"image": "",
-		"userId": "USE100000019",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000023",
-		"content": "Sao mọi người toàn post mấy cái vớ vẩn lên đây thế",
-		"image": "",
-		"userId": "USE100000015",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000024",
-		"content": "Đã 3 tháng rồi mà vẫn chưa có lương, chắc phải kiện lên Bộ thôi",
-		"image": "",
-		"userId": "USE100000017",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000025",
-		"content": "Ko liên quan nhưng có ai mua Holywater ko? Ship đến tận giường",
-		"image": "",
-		"userId": "USE100000067",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000026",
-		"content": "Mình nằm phòng vip mà sao ko có giường nhỉ",
-		"image": "",
-		"userId": "USE100000054",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000027",
-		"content": "Đề nghị sang năm đổi ngay chức viện trưởng",
-		"image": "",
-		"userId": "USE100000012",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000028",
-		"content": "Có thật là có thể chữa khỏi ung thư bằng thiền ko? Bác sĩ xác nhận giùm em",
-		"image": "",
-		"userId": "USE100000033",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000029",
-		"content": "Chết cười với mấy bài post này vãi",
-		"image": "",
-		"userId": "USE100000041",
-		"subPostIds": []
-	},
-
-	{
-		"_id": "POS10000030",
-		"content": "Đọc mấy bài post này đã giúp tôi cười suýt chết",
-		"image": "",
-		"userId": "USE100000082",
+		"postType": 0,
 		"subPostIds": []
 	}
+
 ]
 
 db.post.insert(arr);	
