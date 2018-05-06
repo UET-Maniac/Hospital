683b9dd52a01abf3f6bece2006542aee683c56cd
Nguyenquanguet <nguyenquanguet@gmail.com>
2018-05-06 08:14:08 +0700

    update View Rating

--- SourceCode/Hospital-E/public/js/informationDoctor.js
+++ SourceCode/Hospital-E/public/js/informationDoctor.js
@@ -1,41 +1,7 @@
-// Hover star rating and select star
-$(document).ready(function(){
-    $('#stars li').on('mouseover', function(){
-        var onStar = parseInt($(this).data('value'), 10);
-
-        //Hover star
-        $(this).parent().children('li.star').each(function(e){
-        if (e < onStar) {
-            $(this).addClass('hover');
-        }
-        else {
-            $(this).removeClass('hover');
-        }
-        });
+$('.rating-star').click(function() {
+    $(this).parents('.rating').find('.rating-star').removeClass('checked');
+    $(this).addClass('checked');
         
-    }).on('mouseout', function(){
-        $(this).parent().children('li.star').each(function(e){
-        $(this).removeClass('hover');
-        });
-    });
-
-
-    //Onclick star
-    $('#stars li').on('click', function(){
-        var onStar = parseInt($(this).data('value'), 10);
-        var stars = $(this).parent().children('li.star');
-        
-        for (i = 0; i < stars.length; i++) {
-        $(stars[i]).removeClass('selected');
-        }
-        
-        for (i = 0; i < onStar; i++) {
-        $(stars[i]).addClass('selected');
-        }
-        
-    });
+    var submitStars = $(this).attr('data-value');
+    alert(submitStars);
 });
-
-
-
-// input star from html
